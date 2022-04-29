33578f196ce81089644fe3e29d72af828cae8ce6
Roomsrio <roomsrio7782@gmail.com>
2022-04-29 15:53:58 +0200

    5eme sauv rond souris

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function toggleMenu () {
-    const navbar = document.querySelector('.navbar');
-    const burger = document.querySelector('.burger');
-    burger.addEventListener('click',() => {
-        navbar.classList.toggle('show-nav');
-    })
+function toggleMenu() {
+  const navbar = document.querySelector(".navbar");
+  const burger = document.querySelector(".burger");
+  burger.addEventListener("click", () => {
+    navbar.classList.toggle("show-nav");
+  });
 }
 toggleMenu();
+
+const ronds = document.querySelectorAll(".rond");
+
+window.addEventListener("mousemove", (e) => {
+  ronds.forEach((rond) => {
+    rond.style.left = e.pageX + "px";
+    rond.style.top = e.pageY + "px";
+  });
+});
